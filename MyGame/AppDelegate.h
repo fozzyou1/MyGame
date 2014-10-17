3bf351451ff60fa55b654184e8cb5f64915a1df9
Park <fozzyou1@naver.com>
2014-10-17 13:57:17 +0900

    first update

---
+++ MyGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGame
+//
+//  Created by park on 2014. 10. 17..
+//  Copyright (c) 2014년 park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGame
+//
+//  Created by park on 2014. 10. 17..
+//  Copyright (c) 2014년 park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
